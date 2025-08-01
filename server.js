67647b171785285eea36504977c4eeef31c95e48
SyamKolli2027 <shyamkolli06@gmail.com>
2025-08-01 22:48:39 +0530

    Initial commit

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 // Rate limiting
 const limiter = rateLimit({
